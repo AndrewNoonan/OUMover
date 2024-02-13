9a2cc9bae86bb37c7e629cec3873ceeced1a5a9c
AndrewNoonan <andrewnoonan3@gmail.com>
2024-02-13 14:29:12 -0500

    Adding local code

---
+++ OUMover.ps1
@@ -0,0 +1,16 @@
+﻿#Scan machine S/N into MoverDevices Text file and save. 
+#Press play button in powershell.
+
+foreach ($machine in Get-Content .\MoverDevices.txt) {
+    $machine = "MSL-" + $machine
+    Write-Host "------------- $machine -----------------" -ForegroundColor Cyan
+    Get-ADComputer -Identity $machine | Format-Table Distinguishedname
+    try {
+        Get-ADComputer -Identity $machine | Move-ADObject -TargetPath 'OU=MHS-Client Computers,OU=Migration Staging,DC=act-maxopco2,DC=com'
+        Get-ADComputer -Identity $machine | Format-Table Distinguishedname
+    } catch {
+        Write-Host "You must be an administrator" -ForegroundColor Red
+    }
+}
+
+Read-Host "Press any key to close program"
---
+++ TEST.ps1
@@ -0,0 +1,25 @@
+﻿Get-ADComputer -Identity MSL-PW04B8NT -Properties * | Format-List Name, CanonicalName
+$Machine = Get-ADComputer -Identity MSL-PW04B8NT -Properties * | Format-List CanonicalName | Out-String
+$count = 0
+foreach ($n in $Machine.ToCharArray()) {
+    Write-Host $count")"$n
+    $count++
+}
+$DestinationOU = "MHS-Client Computers/"
+#Scan machine S/N into MoverDevices Text file and save. 
+#Press play button in powershell.
+
+foreach ($machine in Get-Content .\MoverDevices.txt) {
+    $machine = "MSL-" + $machine
+    Write-Host "------------- $machine -----------------" -ForegroundColor Cyan
+
+    $ADRecord = Get-ADComputer -Identity $machine -Properties * | Format-List CanonicalName | Out-String
+    $ADRString = $ADRecord.Substring(55, 33)
+    Write-Host $ADRString
+    if ($ADRString -eq ($DestinationOU + $machine)) {
+        Write-Host "Device location | " -NoNewLine
+        Write-Host $c -ForegroundColor Green
+    }
+}
+
+Read-Host "Press any key to close program"
